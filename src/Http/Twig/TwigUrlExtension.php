ba6b2aa92a4c7f7af3bd63030282fc41b177aab8
Lazare Fortune <lazarefortune@gmail.com>
2024-09-01 20:16:15 +0200

    feat: add event image handler in TwigUrlExtension
    Added a new function 'eventImagePath' in TwigUrlExtension for handling event images. Also, updated the 'index.html.twig' template to use the new function for displaying event images.

--- src/Http/Twig/TwigUrlExtension.php
+++ src/Http/Twig/TwigUrlExtension.php
@@ -3,6 +3,7 @@
 namespace App\Http\Twig;
 
 use App\Domain\Auth\Entity\User;
+use App\Domain\Event\Entity\Event;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelperInterface;
@@ -18,6 +19,7 @@ class TwigUrlExtension extends AbstractExtension
     {
         return [
             new TwigFilter('avatar', $this->avatarPath(...)),
+            new TwigFilter('event_image', $this->eventImagePath(...)),
         ];
     }
 
@@ -28,6 +30,15 @@ class TwigUrlExtension extends AbstractExtension
         }
 
         return $this->uploaderHelper->asset($user, 'avatarFile');
+    }
+
+    public function eventImagePath(Event $event): string
+    {
+        if ($event->getImage() === null) {
+            return '/images/events/default.jpg';
+        }
+
+        return $this->uploaderHelper->asset($event, 'imageFile');
     }
 
 }
